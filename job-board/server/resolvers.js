2ccfa03014a0c10505f9fe446ff678ad4a431e30
Ziellos05 <rolandandresortega@gmail.com>
2022-06-06 16:25:49 -0500

    Mutations done

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,14 +1,23 @@
 import { Job, Company } from './db.js';
 
 export const resolvers = {
+    
     Query: {
       job: (root, { id }) => Job.findById(id),
       jobs: () => Job.findAll(),
       company: (root, { id }) => Company.findById(id),
     },
+
+    Mutation: {
+      createJob: (_root, { input }) => Job.create(input),
+      updateJob: (_root, { input }) => Job.update(input),
+      deleteJob: (_root, { id }) => Job.delete(id)
+    },
+
     Job: {
       company: (job) => Company.findById(job.companyId),
     },
+
     Company: {
       jobs: (company) => Job.findAll((job) => job.companyId === company.id),
     }
